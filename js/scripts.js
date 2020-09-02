ec29e3a7eea6954050568bdeac1639d026dcb03c
Patrick Swaney <prswaney@gmail.com>
2020-09-02 16:34:37 -0700

    finish html and fix errors in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(functioin() {
+$(document).ready(function () {
   $("form#triangleTracker").submit(function (event) {
     event.preventDefault();
     let line1 = ""
@@ -13,25 +13,25 @@ $(document).ready(functioin() {
     if ($("input#line2").val() === "") {
       $('#line2-input').addClass("has-error");
     } else {
-      line1 = parseInt($("input#line2").val());
+      line2 = parseInt($("input#line2").val());
     }
 
     if ($("input#line3").val() === "") {
       $('#line3-input').addClass("has-error");
     } else {
-      line1 = parseInt($("input#line3").val());
+      line3 = parseInt($("input#line3").val());
     }
 
     $('#results').show();
 
-    if (line1 = line2 && line2 = line3) {
-  $('#equilateral').text("Equilateral");
-} else if (line1 = line2 && line3 > line1 + line2 || line1 = line3 && line2 > line1 + line2 || line2 = line3 && line1 > line2 + line3) {
-  $('#isosceles').text("Isosceles");
-} else if (line1 != line2 && line2 != line3 && line3 <= line1 + line2 && line2 <= line1 + line2 && line1 <= line2 + line3) {
-  $('#scalene').text("Scalene");
-} else if (line1 + line2 <= line3 || line1 + line3 <= line2 || line2 + line3 <= line1) {
-  $('#notATriangle').text("Not a triangle");
-}
-})
+    if ((line1 + line2) <= line3 || (line1 + line3) <= line2 || (line2 + line3) <= line1) {
+      $('#triangle').text("not be a");
+    } else if (line1 == line2 && line2 == line3) {
+      $('#triangle').text("be an equilateral");
+    } else if (line1 == line2 || line1 == line3 || line2 == line3) {
+      $('#triangle').text("be an isosceles");
+    } else if ((line1 != line2) && (line2 != line3) && (line1 != line3)) {
+      $('#triangle').text("be a scalene");
+    }
+  })
 });
